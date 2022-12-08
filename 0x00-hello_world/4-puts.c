b6f2ece0925bfb5971dd46500b529717e6bc04bb
Mburu-Stephen <stephenmburu681@gmail.com>
2022-12-08 14:51:00 -0800

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,7 @@
 
 /**
  * main - Entry Point
+ *
  * Return: Always 0 (Success)
  */
 int main void
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,18 +1,10 @@
-#include <stdio.h>                                                                                                                     
-                                                                                                                                       //**                                                                                                                                     
- *
- *  * main - Entry Point                                                                                                             
- *   * Return: Always 0 (Success)                                                                                                        
- *    */                                                                                                                                     
-
-int main void                                                                                                                           
-
-                                                                                                                                        
-
-{                                                                                                                                       
-
-	        puts ("\"Programming is like building a multilingual puzzle");                                                                  
-
-		        return (0);                                                                                                                     
-
-} 
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
