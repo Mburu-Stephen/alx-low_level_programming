1a005ed420bbc87b8feaf5c6fa4bd7e94f02d751
Mburu-Stephen <111083015+Mburu-Stephen@users.noreply.github.com>
2022-12-14 01:35:31 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,16 @@
-#include "main.h"
+#include "holberton.h"
 
-/*
+/**
  * main - entry point
  *
  * Return: 0
  */
-
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u'):
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char *holberton;
+
+	for (holberton = "Holberton\n"; *holberton != '\0'; ++holberton)
+		_putchar(*holberton);
+	return (0);
+}
 
