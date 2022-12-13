6235cc05d3b8d1b4f05f17e329242635ba6a2e57
Mburu-Stephen <111083015+Mburu-Stephen@users.noreply.github.com>
2022-12-14 01:33:07 +0300

    prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,21 @@
-#include "0-putchar.c"
-#include <main.h>
+#include "main.h"
 
-/**
+/*
  * main - entry point
  *
  * Return: 0
  */
+
 int main(void)
 {
-	char 0-putchar*;
+_putchar('_');
+_putchar('p');
+_putchar('u'):
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 
-	for (0-putchar = "0-putchar\n"; *0-putchar != '\0'; ++0-putchar)
-		_putchar(*0-putchar);
-	return (0);
-}
